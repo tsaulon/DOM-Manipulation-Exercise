fcd0d9e1f4cdbdec45489c96e257dd555d72847e
Tyrone <tyrone.saulon@hotmail.com>
2018-03-08 00:18:20 -0500

    DOM Manipulation
    Selecting Tags, changing internal contents, changing tag styles, getting
    and setting attributes.

---
+++ App2/bin/Debug/AppX/js/main.js
@@ -0,0 +1,30 @@
+﻿//DEMO 1
+var firstHeader = document.querySelector("#firstHeader");  //SELECT
+var isBlue = false;
+
+setInterval(function () { //MAINPULATE
+
+    firstHeader.style.background = (isBlue) ? "white" : "#3498db";
+    isBlue = !isBlue;
+}, 1000)
+
+//DEMO 2
+//select <p> tags
+var first = document.querySelector("#first");
+var second = document.getElementsByClassName("special")[1];
+var third = document.getElementsByTagName("p")[2];
+var fourth = document.querySelector("#last");
+
+var showElement = document.querySelector("#showElement");
+
+showElement.innerHTML = fourth.innerHTML;
+
+//DEMO 3
+
+var tag = document.getElementById("highlight"); //SELECT
+
+//MANIPULATE
+function toggleHighlight()
+{
+    tag.classList.toggle("some-class");
+}
